85434bd4ee9ab02fcb731004e31f941f6f81a58c
Logan Sears <lsears322@gmail.com>
2013-05-06 13:58:31 -0600

    Initialized feed_engine rails app with rspec test suite.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedEngine::Application.config.secret_token = '566aae0498e68621d1d191532d7ce8ab3ec7592e36b463fc4419ec62e7bea4b649cf90f195a4b40ee38af8feea45792bab51226ec53ac20343715e86f8a125f2'
