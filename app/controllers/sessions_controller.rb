fcb53e538e22c4fb06b8166ea8e5a6647e37dfe6
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-05-15 10:47:02 -0600

    google sign in creates user, extracts user email and token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,13 @@ class SessionsController < ApplicationController
 
   def create
     info = request.env["omniauth.auth"]
+    user_email = request.env["omniauth.auth"][:info][:email]
+    user_token = request.env["omniauth.auth"][:credentials][:token]
+
     # session[:oauth_token] = info["credentials"]["token"]
     # session[:oauth_secret] = info["credentials"]["secret"]
     user = User.from_omniauth(env["omniauth.auth"])
+    raise user.inspect
     session[:user_id] = user.id
     redirect_to root_url, notice: "Signed in!"
   end
