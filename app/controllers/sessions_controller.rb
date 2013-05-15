16a71a28ffdca7ab16aa5b8cdb5303855c7b1fde
Logan Sears <lsears322@gmail.com>
2013-05-14 19:17:29 -0600

    Nuked untested/experimental methods in order to create a clean slate for TDD

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
+    raise auth.to_yaml
     redirect_to root_url, notice: "Signed in!"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 FeedEngine::Application.routes.draw do
   root to: 'platform#index'
-  get '/auth/google/callback', to: 'sessions#create'
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
   get '/signout', to: 'sessions#destroy', as: 'signout'
   get '/platform/splash', to: 'platform#splash', as: 'splash'
 end
