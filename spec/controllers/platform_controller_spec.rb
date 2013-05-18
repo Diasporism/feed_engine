4bcbfb15660c51f184708fe2538fc9633b229f2e
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-05-18 15:27:04 -0600

    wrote a test for gmail for controller

--- spec/controllers/platform_controller_spec.rb
+++ spec/controllers/platform_controller_spec.rb
@@ -3,25 +3,26 @@ require 'spec_helper'
 describe PlatformController do
 
   let(:token)     { '438712211-PtqdV9KrNBpb4a6dRqAzUb2e7dyoXetJOyhua9MY' }
+  let(:gmail_token) { 'ya29.AHES6ZR-u1-h5Wxk7nAsAm41KYQWXBVKR8iwvihInOKziPXJxFCS0Q' }
   let(:secret)    { 'tRNJ3mTpksSa6XNM8WgJ1SFdIauwpuWCeTdD33WXV0' }
   let(:user) { User.create(email: 'lsears322@gmail.com') }
   let(:user2) { User.create(email: 'lsears32@gmail.com') }
+  let(:user_3) { User.create(email: 'gschooltest@gmail.com') }
   let(:current_user) { controller.current_user }
   let!(:twitter_provider) { user2.providers.create(name: 'twitter',
                                           uid: 438712211,
                                           token: token,
                                           secret: secret) }
-  let!(:google_provider) { user2.providers.create(name: 'google_oauth2',
+  let!(:google_provider) { user_3.providers.create(name: 'google_oauth2',
                                           uid: 438712231,
-                                          token: token,
-                                          secret: secret) }
+                                          token: gmail_token) }
 
   describe '#index' do
 
     context 'When the current user is logged in' do
       context 'but has not added any providers to their feed' do
-        xit 'a gmail client is created' do
-          controller.auto_login(user)
+        it 'a gmail client is created' do
+          controller.auto_login(user_3)
           get :index
           response.response_code.should == 200
         end
